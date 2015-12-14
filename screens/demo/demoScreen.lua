061218545a3aa887cf3a6c750fa1fdd4cb2655cd
Patrick <patrick.rabier@gmail.com>
2015-12-14 22:16:05 +0800

    Adding screens folder

---
+++ screens/demo/demoScreen.lua
@@ -0,0 +1,82 @@
+--[[
+
+   @brief A demo screen
+
+]]
+
+
+require 'Screen'
+require 'Scene'
+require 'SceneObject'
+require 'Vector'
+require 'util'
+
+ShooterScreen = {
+   character = nil,
+   verticalSpeed = 300, -- px/s
+   direction = nil,
+}
+
+-- set up inheritance
+setmetatable(ShooterScreen,{__index = Screen})
+
+-- constructor
+ShooterScreen.__index = ShooterScreen
+
+function ShooterScreen.new()
+   local instance = Screen.new()
+   setmetatable(instance,ShooterScreen)
+   return instance
+end
+
+function ShooterScreen:load()
+   Screen.load(self) -- super
+   self.character = SceneObject.new(
+      'resources/sprites/ship.animation.lua', {
+	 collisionLevel = Scene.COLLISION_LEVELS.FRIENDLY
+   });
+   
+   self.direction = Vector.new(0,0)
+   self.bg = love.graphics.newImage("resources/bg.jpg")
+   self.character:setPosition(20, love.graphics.getHeight() / 2 -  self.character:getFrameHeight() / 2)
+   self.character:setMovementBounds(0, love.graphics.getWidth(), 0, love.graphics.getHeight())
+   self:getScene():addObject(self.character)
+end
+
+function ShooterScreen:keypressed(key) 
+   local direction = self.character:getVector()
+   if key == "up" then
+      self.character:setVector(direction.x, direction.y - 1)
+   elseif key == "down" then
+      self.character:setVector(direction.x, direction.y + 1)
+   elseif key == "right" then
+      self.character:setVector(direction.x + 1, direction.y)
+   elseif key == "left" then
+      self.character:setVector(direction.x - 1, direction.y)
+   end
+end
+
+function ShooterScreen:keyreleased(key) 
+   local direction = self.character:getVector()
+   if key == "up" then
+      self.character:setVector(direction.x, direction.y + 1)
+   elseif key == "down" then
+      self.character:setVector(direction.x, direction.y - 1)
+   elseif key == "right" then
+      self.character:setVector(direction.x - 1, direction.y)
+   elseif key == "left" then
+      self.character:setVector(direction.x + 1, direction.y)
+   end
+end
+
+function ShooterScreen:update(dt)
+   return Screen.update(self, dt) -- super
+end
+
+function ShooterScreen:draw()
+   -- Background
+   love.graphics.draw(self.bg, 0, 0)
+   return Screen.draw(self)
+end
+
+return ShooterScreen
